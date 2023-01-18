c5a1d1ce85e7e390ccbb59b472e4b9eaa27b2947
herror <tom.leboeuf0798@gmail.com>
2023-01-18 18:23:54 +0100

    adding files

--- lua/after/plugin/colors.lua
+++ lua/after/plugin/colors.lua
@@ -26,6 +26,8 @@ vim.g.gruvbox_baby_keyword_style = "italic"
 vim.g.gruvbox_baby_telescope_theme = 1
 
 -- Enable transparent mode
-vim.g.gruvbox_baby_transparent_mode = 0
+vim.g.gruvbox_baby_transparent_mode = 1
 -- Load the colorscheme
 vim.cmd[[colorscheme gruvbox-baby]]
+ColorMyPencils()
+ChangeBackgroundColor("dark")
