df244589cfd3e0e27761e97707009e6eada3c3eb
tleboeuf <tleboeuf@i-tracing.com>
2023-05-09 11:28:29 +0200

    config du taff, rust + hint + format on save

--- lua/after/plugin/telescope.lua
+++ lua/after/plugin/telescope.lua
@@ -2,7 +2,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ 
+	builtin.grep_string({
 		search = vim.fn.input("Grep > ")
 	});
 end)
