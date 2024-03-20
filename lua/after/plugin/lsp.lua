dc016f2ceb84086cdef0fe23c1484fd71d7a7301
tleboeuf <tleboeuf@i-tracing.com>
2024-03-20 11:06:51 +0100

    modif lsp-zero confs, binds etc

--- lua/after/plugin/lsp.lua
+++ lua/after/plugin/lsp.lua
@@ -35,13 +35,17 @@ lsp.setup_nvim_cmp({
 
 })
 
+lsp.set_sign_icons({
+	error = '✘',
+	warn = '▲',
+	hint = '⚑',
+	info = '»'
+})
 
 lsp.on_attach(function(client, bufnr)
-	lsp_format_on_save(bufnr)
+	lsp.default_keymaps({ buffer = bufnr })
+	lsp.buffer_autoformat()
 end)
-
-
-lsp.setup()
 
 vim.diagnostic.config({
 	virtual_text = false,
@@ -51,3 +55,27 @@ vim.diagnostic.config({
 	severity_sort = true,
 	float = true,
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	desc = 'LSP actions',
+	callback = function(event)
+		local opts = { buffer = event.buf }
+
+		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+		vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+		vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+		vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+		vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+		vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+		vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+		vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+		vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+		vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+		vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+		vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+	end
+})
+
+lsp.setup()
